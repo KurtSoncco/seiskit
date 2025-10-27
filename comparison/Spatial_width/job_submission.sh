a23d0ad62b5fc9c0eb949444469ba3d05e04ac4b
Kurt Soncco <kurtwal98@berkeley.edu>
2025-10-27 13:19:15 -0700

    fix: Correct formatting of SLURM job submission script

--- comparison/Spatial_width/job_submission.sh
+++ comparison/Spatial_width/job_submission.sh
@@ -6,8 +6,8 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --time=00:00:30
-<strong>#SBATCH --array=0-31</strong>
-<strong>#SBATCH --output=array_job_%A_task_%a.out</strong>
-<strong>#SBATCH --error=array_job_%A_task_%a.err</strong>
+#SBATCH --array=0-31
+#SBATCH --output=array_job_%A_task_%a.out
+#SBATCH --error=array_job_%A_task_%a.err
 ## Command(s) to run:
 echo "I am task $SLURM_ARRAY_TASK_ID"
