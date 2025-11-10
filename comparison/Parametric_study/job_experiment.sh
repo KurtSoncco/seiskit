a4f9a804d0971bd5275bbcdc5c5929c534fb2136
Kurt Soncco <kurtwal98@berkeley.edu>
2025-11-09 20:36:21 -0800

    Correct name of exclusion

--- comparison/Parametric_study/job_experiment.sh
+++ comparison/Parametric_study/job_experiment.sh
@@ -9,7 +9,7 @@
 #SBATCH --array=0-44%10
 #SBATCH --output=logs/array_job_%A_task_%a.out
 #SBATCH --error=logs/array_job_%A_task_%a.err
-#SBATCH --exclude=n0029
+#SBATCH --exclude=n0029.savio3
 
 set -euo pipefail
 
