dd70b7f472407835e706fbcd0a769f2014d09561
Kurt Soncco <kurtwal98@berkeley.edu>
2025-12-09 20:16:10 -0800

    Update kohmachi function parameters for improved frequency response
    - Increased the frequency parameter in the `kohmachi` function calls from 150 to 500 in the TTF function, enhancing the accuracy of the transfer function calculations for both surface and bedrock responses.

--- seiskit/ttf/TTF.py
+++ seiskit/ttf/TTF.py
@@ -54,8 +54,8 @@ def TTF(surface_acc, base_acc, dt=1e-4, n_points=1000, Vsmin=None, dz: float = 5
     freq = np.logspace(np.log10(0.1), np.log10(fmax), n_points)
 
     # get TF
-    kohmachi_s = kohmachi(FAS_s, freq, 150)
-    kohmachi_b = kohmachi(FAS_b, freq, 150)
+    kohmachi_s = kohmachi(FAS_s, freq, 500)
+    kohmachi_b = kohmachi(FAS_b, freq, 500)
 
     # Handle division by zero by adding a small epsilon
     epsilon = 1e-12
