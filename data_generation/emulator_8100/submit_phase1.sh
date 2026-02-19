16cf2ffcf37344d03e37dc9e03e580df6f47b149
Kurt Soncco <kurtwal98@berkeley.edu>
2026-02-19 14:13:27 -0800

    Final jobs to be done

--- data_generation/emulator_8100/submit_phase1.sh
+++ data_generation/emulator_8100/submit_phase1.sh
@@ -4,4 +4,4 @@
 # Pass --array to override script default and run only these tasks.
 cd "$(dirname "$0")"
 mkdir -p logs
-exec sbatch --array=108,111 phase1_savio2.sh "$@"
+exec sbatch --array=115-117 phase1_savio2.sh "$@"
